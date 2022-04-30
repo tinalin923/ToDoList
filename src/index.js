6375291ddaf00fbfc06aab0e3300b55fa12e8d4b
tinalin923 <tinalin923@gmail.com>
2022-04-30 16:07:35 +0800

    change the render method

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 
