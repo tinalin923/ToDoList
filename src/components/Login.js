60cf312dc54c4a7d1ed0c8b174e55d5b05afee44
tinalin923 <tinalin923@gmail.com>
2022-05-08 23:15:02 +0800

    use useRef

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { auth } from '../firebase/firebaseConfig.js';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
@@ -7,14 +7,13 @@ import { useNavigate } from 'react-router-dom';
 export default function Login() {
 	const [loading, setLoading] = useState(false);
 	const navigate = useNavigate();
-	
-	const handleSubmit = async(e) => {
-		e.preventDefault();
-		const { email, password } = e.target.elements
+	const emailRef = useRef('');
+	const passwordRef = useRef('');
 
+	const handleSubmit = async() => {
+		setLoading(true);
 		try {
-			setLoading(true);
-			const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+			const userCredential = await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
 			console.log(userCredential);
 			navigate('/list');
 		} catch (e) {
@@ -27,11 +26,11 @@ export default function Login() {
   return (
 		<>
 			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<input name='email' placeholder='email' type='email' required/>
-				<input name='password' placeholder='password' type='password' required/>
-				<button type='submit' disabled={loading} >Login</button>
-			</form>
+			<div>
+				<input ref={emailRef} name='email' placeholder='email' type='email' required/>
+				<input ref={passwordRef} name='password' placeholder='password' type='password' required/>
+				<button type='submit' disabled={loading} onClick={handleSubmit}>Login</button>
+			</div>
 		</>
 	)
 }
